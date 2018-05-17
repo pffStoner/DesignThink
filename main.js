6bd4a5a66aaf5870145e00164eed055f5e7b38d4
Dimitar Kokotanekov <acid901@mail.bg>
2018-05-17 22:13:00 +0300

    added future projects section

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ $(window).scroll(function(){
     }
 });
 
+ // init Isotope
+ let $grid = $('.picture-container').isotope({
+    // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+    let filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    });
+
